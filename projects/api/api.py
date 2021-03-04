3f59a8a462cac7d54d4c18640fdcc744345f62ad
Utkarsh <trainee13@indovisionservices.in>
2021-03-04 10:47:21 +0530

    added new project python-getting-started

--- projects/api/api.py
+++ projects/api/api.py
@@ -1,11 +1,20 @@
-import flask
-
-app = flask.Flask(__name__)
+from flask import Flask,render_template
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route("/utkarsh")
+def utkarsh():
+	return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+# if __name__ == "__api__":
+#      app.run(debug=True)
 app.run()
